829c3e7dd5255927d74169938544f70c43a2f9e9
sandro_herbst <sandro.herbst@gmail.com>
2019-11-11 22:41:59 -0300

    Acessando o servidor da API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
+import axios from 'axios'
 import { 
   BrowserRouter as Router,
   Route
@@ -14,12 +15,19 @@ const Genres = () => {
 }
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
   return (
     <Router>
       <div>
         <Header />
         <Route path='/' exact component={Home}/>
         <Route path='/genres' component={Genres}/>
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
