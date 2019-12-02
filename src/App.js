bc41d111e1ecd945f66603a4a87c41c88f0dc638
sandro_herbst <sandro.herbst@gmail.com>
2019-12-02 15:07:42 -0300

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
-import axios from 'axios'
 import { 
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 import Genres from './Genres'
 import NewGenre from './NewGenre'
@@ -14,26 +14,20 @@ const Home = () => {
 }
 
 
-
 function App() {
-  const [data, setData] = useState({})
-  useEffect(() => {
-    axios.get('/api').then(res => {
-      setData(res.data)
-    })
-  }, [])
   return (
     <Router>
       <div>
         <Header />
-        <Route path='/' exact component={Home}/>
-        <Route path='/genres' exact component={Genres}/>
-        <Route path='/genres/new' exact component={NewGenre}/>
-        <Route path='/genres/:id' exact component={EditGenre}/>
-        <pre>{JSON.stringify(data)}</pre>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/genres' exact component={Genres}/>
+          <Route path='/genres/new' exact component={NewGenre}/>
+          <Route path='/genres/:id' exact component={EditGenre}/>
+        </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
 export default App
